5c70cd4e7b2df8414612c3179331c041c8a0dccc
Bogdan Liutikov <lbi2001@yandex.ru>
2021-06-10 20:37:21 +0300

    some changes

--- src/GaussSeidelMethod/Main.java
+++ src/GaussSeidelMethod/Main.java
@@ -18,14 +18,11 @@ public class Main {
         int status = matrix.solve();
 
         if (status == Matrix.SOLVED) {
-            file.write("Матрица решена!\n");
             System.out.println("Матрица решена!\n");
             answ = matrix.getSolution();
             for (int i = 0; i < answ.length; i++)
                 System.out.printf("%15.6E", answ[i]);
-            file.write(answ);
         } else {
-            file.write("Матрицу невозможно решить методом Гаусса – Зейделя!\n");
             System.out.println("Матрицу невозможно решить методом Гаусса – Зейделя!\n");
         }
 
--- src/GaussSeidelMethod/Matrix.java
+++ src/GaussSeidelMethod/Matrix.java
@@ -1,11 +1,10 @@
 package GaussSeidelMethod;
 
-import java.util.Arrays;
-
 public class Matrix {
 
     private double[][] matrix;
     private double[] solution;
+    private double[] absRowSum;
     private int n, m;
     private double EPS;
     public final static int SOLVED = 0;
@@ -16,19 +15,23 @@ public class Matrix {
         this.matrix = file.readMatrix();
         n = matrix.length;
         m = matrix[0].length;
+        absRowSum = new double[n];
     }
 
     public int solve() {
         int status = SOLVED;
 
-        //Проверяем на наличие нулей на диагонали
-        if (!diagonalZero()) {
+        int nextStep = diagonalPredominance();
+        final int diagonalZero = -1;
+        final int diagonalPredominance = 0;
+        final int nondiagonalPredominance = 1;
 
-            //Проверяем на ДУС
-            if (diagonalPredominance()) {
-                // Решение
+        //Присутствуют ли диагональные нули
+        if (nextStep != diagonalZero) {
+            // Решение
+            if (nextStep == diagonalPredominance) {
                 double[] x = new double[n];
-                solution = GaussSeidel(x);
+                status = GaussSeidel(x);
             } else {
                 // Решение с контролем
                 status = controlSolve();
@@ -37,25 +40,34 @@ public class Matrix {
 
             //Пытаемся привести матрицу к виду с диагональным преобладанием
             System.out.println("Будет произведена попытка приведения матрицы к матрице с диагональным преобладанием");
-            //modifyToDiagonalPredominance();
             if (makeDominant())
-                System.out.print("Удалось ");
-            else
-                System.out.print("Не удалось ");
-            System.out.println("привести матрицу к матрице с диагональным преобладанием\n");
+                System.out.println("Удалось привести матрицу к матрице с диагональным преобладанием");
+            else {
+                System.out.println("Не удалось привести матрицу к матрице с диагональным преобладанием");
+                System.out.println("Попробуем просто убарать нули с диагонали.");
+
+                print();
+                System.out.println();
+                if (killZero()) {
+                    System.out.print("Удалось ");
+                    System.out.println();
+                } else {
+                    System.out.print("Не удалось ");
+                    System.out.println("убарть нули с диагонали\n");
+                }
+            }
 
             print();
 
-            //Проверяем на наличие нулей на диагонали
-            if (diagonalZero()) {
+            nextStep = diagonalPredominance();
+            if (nextStep == nondiagonalPredominance)
                 return UNSOLVABLE;
-            } else {
-
+            else {
                 //Проверяем на ДУС
-                if (diagonalPredominance()) {
+                if (nextStep == diagonalPredominance) {
                     // Решение
                     double[] x = new double[n];
-                    solution = GaussSeidel(x);
+                    status = GaussSeidel(x);
                 } else {
                     // Решение с контролем
                     status = controlSolve();
@@ -66,31 +78,35 @@ public class Matrix {
         return status;
     }
 
-    private double[] GaussSeidel(double[] initialApproximation) {
+    private int GaussSeidel(double[] initialApproximation) {
 
         double[] x = initialApproximation;
         boolean completionCondition = false;
-        double prevX;
+        double diff = 0;
         int iterations = 0;
-        while (!completionCondition && iterations < 1/EPS) {
+        while (!completionCondition && iterations < 1 / EPS) {
             iterations++;
-            completionCondition = true;
-            for (int i = 0; i < n; i++) {
-                prevX = x[i];
-                x[i] = (matrix[i][m - 1] - otherXSum(i, x)) / matrix[i][i];
-                if (Math.abs(x[i] - prevX) < EPS)
-                    continue;
-                completionCondition = false;
-            }
+            diff = nextIteration(x);
+            if (diff < EPS)
+                completionCondition = true;
         }
 
-        if(iterations == 1/EPS) {
-            System.out.println("Метод не сходится!!!\n" +
-                    "Ответ не имеет смысла!\n");
-            return x;
+        if (iterations == 1 / EPS)
+            return UNSOLVABLE;
+
+        solution = x;
+        return SOLVED;
+    }
+
+    private double nextIteration(double[] currentIteration) {
+        double[] x = currentIteration;
+        double diff = 0, prevX;
+        for (int j = 0; j < n; j++) {
+            prevX = x[j];
+            x[j] = (matrix[j][this.m - 1] - otherXSum(j, x)) / matrix[j][j];
+            diff = Math.max(Math.abs(x[j] - prevX), diff);
         }
-        System.out.printf("Метод получил ответ за %d итераций\n", iterations);
-        return x;
+        return diff;
     }
 
     /**
@@ -107,67 +123,9 @@ public class Matrix {
         return sum;
     }
 
-    private void modifyToDiagonalPredominance() {
-
-        System.out.println("Будет произведена попытка приведения матрицы к виду с диагональным преобладанием.\n");
-
-        int index, numberOfMax;
-        double max;
-        boolean[] flexable = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            flexable[i] = true;
-        }
-
-        for (int z = 0; z < m - 1; z++) {
-            for (int i = 0; i < m - 1; i++) {
-
-                max = -1;
-                index = -1;
-                numberOfMax = 0;
-                for (int j = 0; j < n; j++) {
-
-                    if (!flexable[j])
-                        continue;
-
-                    if (Math.abs(matrix[j][i]) > max) {
-                        max = Math.abs(matrix[j][i]);
-                        index = j;
-                    }
-                }
-
-                for (int j = 0; j < n; j++) {
-
-                    if (!flexable[j])
-                        continue;
-
-                    if (Math.abs(matrix[j][i]) == max)
-                        numberOfMax++;
-                }
-
-                flexable[i] = numberOfMax > 1; // если максимумов в столбце > 1, то строка flexable
-
-                if (!flexable[i] && index >= 0)
-                    swapRows(i, index);
-            }
-        }
-
-        int l;
-        for (l = 0; l < n; l++) {
-            if (!flexable[l])
-                break;
-        }
-        if (l == n) {
-            System.out.println("Нет такой перестановки\n");
-            return;
-        }
-
-        print();
-    }
-
-    public boolean transformToDominant(int r, boolean[] visited, int[] rows) {
+    private boolean modifyToDiagonalPredominance(int r, boolean[] visited, int[] rows) {
         int n = this.n;
-        if (r == this.n) {
+        if (r == n) {
             double[][] T = new double[n][m];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++)
@@ -182,11 +140,12 @@ public class Matrix {
         for (int i = 0; i < n; i++) {
             if (visited[i]) continue;
 
-            if (absRowSum(matrix[i]) - 2 * Math.abs(matrix[i][r]) < EPS) { // diagonally dominant?
+
+            if (absRowSum(i) - 2 * Math.abs(matrix[i][r]) <= EPS) {
                 visited[i] = true;
                 rows[r] = i;
 
-                if (transformToDominant(r + 1, visited, rows))
+                if (modifyToDiagonalPredominance(r + 1, visited, rows))
                     return true;
 
                 visited[i] = false;
@@ -198,13 +157,49 @@ public class Matrix {
     /**
      * @return true если матрицу возможно преобразовать к матрице с диагональным преобладанием
      */
-    public boolean makeDominant() {
+    private boolean makeDominant() {
+        boolean[] visited = new boolean[n];
+        int[] rows = new int[n];
+
+        return modifyToDiagonalPredominance(0, visited, rows);
+    }
+
+    private boolean killZero() {
         boolean[] visited = new boolean[matrix.length];
         int[] rows = new int[matrix.length];
 
-        Arrays.fill(visited, false);
+        return tryToKillZero(0, visited, rows);
+    }
 
-        return transformToDominant(0, visited, rows);
+    private boolean tryToKillZero(int r, boolean[] visited, int[] rows) {
+        int n = this.n;
+        if (r == n) {
+            double[][] T = new double[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++)
+                    T[i][j] = matrix[rows[i]][j];
+            }
+
+            matrix = T;
+
+            return true;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+
+
+            if (Math.abs(matrix[i][r]) > EPS) {
+                visited[i] = true;
+                rows[r] = i;
+
+                if (tryToKillZero(r + 1, visited, rows))
+                    return true;
+
+                visited[i] = false;
+            }
+        }
+        return false;
     }
 
     private void swapRows(int i, int index) {
@@ -225,58 +220,53 @@ public class Matrix {
 
         double[] x = new double[n];
         int i;
-        double prevX, diff = 0;
-        double prevdiff = diff;
+        double diff = 0;
+        double prevdiff;
         int counter = 0;
 
         // Следим за тем, чтобы разница монтонно уменьшалась
         for (i = 0; i < k; i++) {
-            for (int j = 0; j < n; j++) {
-                prevX = x[j];
-                x[j] = (matrix[j][this.m - 1] - otherXSum(j, x)) / matrix[j][j];
-                diff = Math.max(Math.abs(x[j] - prevX), diff);
-            }
-            counter++;
-            if (prevdiff - diff < 0)
-                counter = 0;
             prevdiff = diff;
+            diff = nextIteration(x);
+
+            counter++;
+            if (diff > prevdiff)
+                counter = 0;
         }
 
         if (counter < m)
             return UNSOLVABLE;
 
         //Отдаём нынешние корни на дорешивание
-        solution = GaussSeidel(x);
-        return SOLVED;
+        return GaussSeidel(x);
     }
 
     /**
-     * Возвращает true, если хотя бы один элемент диагонали равен нулю
-     * Возвращает false, если на диагонали нет нулевых элементов
-     *
-     * @return Присутвуют ли на диагонали нули
+     * @return <ul>
+     * <li>-1 - На диагонали присутсвует 0</li>
+     * <li> 0 - Матрица с диагональным преобладанием</li>
+     * <li> 1 - Матрица не имеет свойства диагонального преобладания</li>
+     * </ul>
      */
-    private boolean diagonalZero() {
+    private int diagonalPredominance() {
+        fillAbsRowSum();
         for (int i = 0; i < n; i++) {
             if (Math.abs(matrix[i][i]) < EPS)
-                return true;
-        }
-        return false;
-    }
-
-    /**
-     * @return Является ли матрица матрицей с диагональным преобладанием.
-     */
-    private boolean diagonalPredominance() {
-        for (int i = 0; i < n; i++) {
+                return -1;
             if (!diagonalPredominanceRow(i))
-                return false;
+                return 1;
         }
-        return true;
+        return 0;
     }
 
     private boolean diagonalPredominanceRow(int row) {
-        return absRowSum(matrix[row]) - 2 * Math.abs(matrix[row][row]) < EPS;
+        return absRowSum(row) - 2 * Math.abs(matrix[row][row]) <= EPS;
+    }
+
+    private void fillAbsRowSum() {
+        for (int i = 0; i < n; i++) {
+            absRowSum[i] = absRowSum(matrix[i]);
+        }
     }
 
     private double absRowSum(double[] row) {
@@ -285,6 +275,10 @@ public class Matrix {
             sum += Math.abs(row[i]);
         }
         return sum;
+    }
+
+    private double absRowSum(int indexRow) {
+        return absRowSum[indexRow];
     }
 
     public double[] getSolution() {
@@ -301,11 +295,4 @@ public class Matrix {
         System.out.println();
     }
 
-//    private double eps() {
-//        double eps = 1.0;
-//        while (1 + eps > 1) {
-//            eps = eps / 10;
-//        }
-//        return eps;
-//    }
 }
